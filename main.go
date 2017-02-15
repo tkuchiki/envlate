a5cbe40e06764eb7948f69986e76f01b62734830
tkuchiki <kuchiki.taku@gmail.com>
2017-02-15 15:11:39 +0900

    add split

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
-	"text/template"
 )
 
 func getFp(filename string) (*os.File, error) {
@@ -44,30 +41,6 @@ func getEnvMap() map[string]string {
 	return envs
 }
 
-func renderTemplate(tplb []byte, envs map[string]string, unsetErr bool) (string, error) {
-	tpl, err := template.New("").Parse(string(tplb))
-	if err != nil {
-		return "", err
-	}
-
-	option := "zero"
-	if unsetErr {
-		option = "error"
-	}
-
-	var txt bytes.Buffer
-	err = tpl.Option("missingkey="+option).Execute(&txt, envs)
-	if err != nil {
-		re := regexp.MustCompile(`template: :([0-9]+):([0-9]+): executing .+: map has no entry for key "(.+)"`)
-		group := re.FindStringSubmatch(err.Error())
-		if len(group) != 4 {
-			return "", err
-		}
-		err = fmt.Errorf("line %s char %s : no entry for key `%s`", group[1], group[2], group[3])
-	}
-	return txt.String(), err
-}
-
 func stringToFileMode(mode string) (os.FileMode, error) {
 	m, err := strconv.ParseInt(mode, 8, 0)
 	if err != nil {
@@ -91,7 +64,7 @@ var (
 
 func main() {
 	kingpin.CommandLine.Help = "Expand environment variables in template (the templates use Go text/template syntax)"
-	kingpin.Version("0.1.1")
+	kingpin.Version("0.1.2")
 	kingpin.Parse()
 
 	f, ferr := getFp(*file)
@@ -107,7 +80,9 @@ func main() {
 
 	envs := getEnvMap()
 
-	txt, terr := renderTemplate(b, envs, *unset)
+	t := NewTemplate(*unset)
+
+	txt, terr := t.render(b, envs)
 	if terr != nil {
 		log.Fatal(terr)
 	}
